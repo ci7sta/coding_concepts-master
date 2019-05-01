66d4890d704dc44c96b10f427396aafd8f467b54
Callum Imrie <ci7@st-andrews.ac.uk>
2019-05-01 15:10:11 +0100

    Can now specify number of moves to do (optionally)

--- src/DirectionControls.js
+++ src/DirectionControls.js
@@ -36,16 +36,40 @@ class DirectionControls extends Component {
     }
 
     handleInput() {
-        var input = this.refs.commandInput.value.toLowerCase().replace(/ /g,'');
+        var input = this.refs.commandInput.value.toLowerCase().replace(/ /g, '');
 
-        if (input === "forward();") {
-            this.forwardClick();
-        } else if (input === "back();") {
-            this.backwardClick();
-        } else if (input === "left();") {
-            this.leftClick();
-        } else if (input === "right();") {
-            this.rightClick();
+        var number = parseInt(input.replace(/[^\d.]/g, ''), 10);
+        var text = input.replace(/[0-9]/g, '');
+
+        if (text === "forward();") {
+
+            if (isNaN(number)) {
+                this.forwardClick();
+            }
+            for (var i = 0; i < number; i++) {
+                this.forwardClick();
+            }
+        } else if (text === "back();") {
+            if (isNaN(number)) {
+                this.backwardClick();
+            }
+            for (var i = 0; i < number; i++) {
+                this.backwardClick();
+            }
+        } else if (text === "left();") {
+            if (isNaN(number)) {
+                this.leftClick();
+            }
+            for (var i = 0; i < number; i++) {
+                this.leftClick();
+            }
+        } else if (text === "right();") {
+            if (isNaN(number)) {
+                this.rightClick();
+            }
+            for (var i = 0; i < number; i++) {
+                this.rightClick();
+            }
         } else {
             this.refs.commandInput.value = input + " is not correct.";
         }

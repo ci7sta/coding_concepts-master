54065308bef050d08f115e2745a63c37a1dc7ff5
Callum Imrie <ci7@st-andrews.ac.uk>
2019-05-01 15:18:39 +0100

    Can now chain together commands, separated by semicolon

--- src/DirectionControls.js
+++ src/DirectionControls.js
@@ -38,40 +38,48 @@ class DirectionControls extends Component {
     handleInput() {
         var input = this.refs.commandInput.value.toLowerCase().replace(/ /g, '');
 
-        var number = parseInt(input.replace(/[^\d.]/g, ''), 10);
-        var text = input.replace(/[0-9]/g, '');
 
-        if (text === "forward();") {
+        var commands = input.split(";");
+        debugger;
 
-            if (isNaN(number)) {
-                this.forwardClick();
+        for (var commandIndex = 0; commandIndex < commands.length; commandIndex++) {
+            var number = parseInt(commands[commandIndex].replace(/[^\d.]/g, ''), 10);
+            var text = commands[commandIndex].replace(/[0-9]/g, '');
+
+            if (text === "forward()") {
+
+                if (isNaN(number)) {
+                    this.forwardClick();
+                }
+                for (var i = 0; i < number; i++) {
+                    this.forwardClick();
+                }
+            } else if (text === "back()") {
+                if (isNaN(number)) {
+                    this.backwardClick();
+                }
+                for (var i = 0; i < number; i++) {
+                    this.backwardClick();
+                }
+            } else if (text === "left()") {
+                if (isNaN(number)) {
+                    this.leftClick();
+                }
+                for (var i = 0; i < number; i++) {
+                    this.leftClick();
+                }
+            } else if (text === "right()") {
+                if (isNaN(number)) {
+                    this.rightClick();
+                }
+                for (var i = 0; i < number; i++) {
+                    this.rightClick();
+                }
+            } else if (text === "") {
+                return;
+            } else {
+                this.refs.commandInput.value = input + " is not correct.";
             }
-            for (var i = 0; i < number; i++) {
-                this.forwardClick();
-            }
-        } else if (text === "back();") {
-            if (isNaN(number)) {
-                this.backwardClick();
-            }
-            for (var i = 0; i < number; i++) {
-                this.backwardClick();
-            }
-        } else if (text === "left();") {
-            if (isNaN(number)) {
-                this.leftClick();
-            }
-            for (var i = 0; i < number; i++) {
-                this.leftClick();
-            }
-        } else if (text === "right();") {
-            if (isNaN(number)) {
-                this.rightClick();
-            }
-            for (var i = 0; i < number; i++) {
-                this.rightClick();
-            }
-        } else {
-            this.refs.commandInput.value = input + " is not correct.";
         }
     }
 
